d32b41449c86b5ce7ce06f091ead4952c1f376b6
Chris Frisz <chris.frisz@gmail.com>
2013-11-21 23:37:15 -0500

    Changing repl namespace to syntax-case.psyntax.

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
     "An implementation of the syntax-case syntax transormer in Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :repl-options {:init-ns syntax-case.psyntax})
